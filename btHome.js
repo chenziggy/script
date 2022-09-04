c38eec937569acd06dd9ad5ea528df3ec1d24b92
Ziggy Chen <ziggy.chen@aylaasia.com>
2022-09-04 16:15:37 +0800

    feat: 删除遮罩，增加匹配网站

--- btHome.js
+++ btHome.js
@@ -4,18 +4,32 @@
 // @version      1.0
 // @description  try to take over the world!
 // @author       Ziggy chen
-// @match        https://www.btbtt15.com/*
+// @match        *://*.btbtt11.com/*
+// @match        *://*.btbtt12.com/*
+// @match        *://*.btbtt13.com/*
+// @match        *://*.btbtt14.com/*
+// @match        *://*.btbtt15.com/*
+// @match        *://*.btbtt16.com/*
+// @match        *://*.btbtt17.com/*
+// @match        *://*.btbtt18.com/*
+// @match        *://*.btbtt19.com/*
+// @match        *://*.btbtt20.com/*
 // @icon         https://www.btbtt15.com/favicon.ico
-// @grant        none
+// @run-at       document-start
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
   "use strict";
-  let arr = document.querySelectorAll("div.width");
-  console.log("arr", arr.length);
-  let last = arr[arr.length - 1];
-  if (last) {
-    last.style.width = "auto";
-    last.style.maxWidth = "1500px";
-  }
+  // 增加宽度
+  GM_addStyle(".width {max-width:1500px !important; width:auto !important;}");
+
+  // 删除左右两侧无法点击的遮罩
+  document.onreadystatechange = function () {
+    if (document.readyState === "interactive") {
+      document
+        .querySelectorAll(".wrapper_bg_c.hidden-xs")
+        .forEach((el) => el.remove());
+    }
+  };
 })();
