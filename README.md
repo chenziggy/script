0e355b5aa06c6954114867b1c5edb695f78941e6
Ziggy Chen <ziggy.chen@aylaasia.com>
2022-10-03 18:08:51 +0800

    feat: vue.js 去掉 banner高度

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 #### bt之家脚本
 
 - 列表maxWidth:1500px
+
+### vue.org脚本
+
+- 去掉banner高度，adguard规则过滤广告
---
+++ vue.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         vue.js
+// @namespace    ziggy chen
+// @license MIT
+// @version      1.1
+// @description  vue.org script
+// @author       Ziggy chen
+// @match        *://*.vuejs.org/*
+// @icon         https://v2.cn.vuejs.org/images/logo.svg
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  // banner 高度  0
+  GM_addStyle("html:not(.banner-dismissed) {--vt-banner-height: 0px}");
+
+  // 删除 banner 报错  使用 adguard 过滤banner广告
+  // document.onreadystatechange = function () {
+  //   if (document.readyState === "interactive") {
+  //     const banner = document.querySelectorAll(".banner");
+  //     banner.forEach((el) => {
+  //       if (el && el.parentNode) {
+  //         el.parentNode.repalce(el);
+  //       }
+  //     });
+  //   }
+  // };
+})();
