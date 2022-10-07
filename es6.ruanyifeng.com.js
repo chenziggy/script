c5ceaca7cc4d17b163491fa6177b499a8a6f4c31
Ziggy Chen <ziggycs9796@hotmail.com>
2022-10-07 13:26:52 +0800

    feat: 新增es6文档脚本

---
+++ es6.ruanyifeng.com.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         es6.ruanyifeng
+// @namespace    ziggy chen
+// @license MIT
+// @version      1.2
+// @description  vue.org script
+// @author       Ziggy chen
+// @match        *://es6.ruanyifeng.com/*
+// @icon         https://v2.cn.vuejs.org/images/logo.svg
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  // content 高度  800
+  GM_addStyle(
+    "@media (min-width: 40rem) { #content { width: 800px !important; }}"
+  );
+  GM_addStyle(
+    "#back_to_top { right: 25px!important;margin-left: 0 !important;}"
+  );
+  GM_addStyle("#edit { right: 25px!important;margin-left: 0 !important;}");
+  GM_addStyle("#theme { right: 25px!important;margin-left: 0 !important;}");
+})();
